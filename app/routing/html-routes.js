b280699b3cd1cde29b7c1e07190084c45eceeb05
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-10-03 22:27:46 -0400

    added links so friends.js can be displayed when link on home page is clicked 9like in the example

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -14,12 +14,22 @@ module.exports = function(app){
 		console.log("Path to folders: " + path.join(__dirname));
 	});
 
+	// A GET Route to `/friends` which should display when API Table link is clicked (like in example).
+	app.get('/friends', function (req, res) {
+		res.sendFile(path.join(__dirname, '/../data/friends.js'));
+		console.log("Path to folders: " + path.join(__dirname));
+	});
+
 	// A default USE route that leads to `home.html` which displays the home page.
 	app.use(function(req, res){
 	   res.sendFile(path.join(__dirname, '/../public/home.html'));
 	});
 
+
 }
+
+
+
 
 
 
